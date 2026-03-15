cad92a1b4051be1c1a25ebd77eed1eca9c12186a
Manideep Jammigumpula <manideepj2@gmail.com>
2026-03-14 23:13:49 -0400

    add intro

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { TextField, Button, Divider } from "@mui/material";
+import { TextField, Button, Divider, Typography } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
@@ -13,8 +13,25 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="w-[400px] bg-white p-8 rounded-xl shadow-lg">
+    <div className="flex items-center justify-center h-screen">
+      <div className="p-8 max-w-3xl">
+        <h2 className="text-2xl font-semibold text-center mb-6">TravelBuddy</h2>
+        <Typography className="text-justify">
+          <code>TravelBuddy</code> is a platform designed to help people find
+          companions for their journeys by connecting travelers who are heading
+          in the same direction at the same time. The idea is to make travel
+          more social and collaborative by allowing individuals to either look
+          for a travel buddy for a trip they are taking or offer an available
+          seat in their car to someone traveling along the same route. <br />
+          <br />
+          Whether someone is flying to a destination or driving between cities,
+          the platform helps match them with others on a similar journey. By
+          bringing together people with shared travel plans, TravelBuddy aims to
+          make trips more enjoyable, safer, and more economical while ncouraging
+          meaningful connections between travelers.
+        </Typography>
+      </div>
+      <div className="w-[400px] p-8 rounded-xl shadow-lg">
         <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
 
         <div className="space-y-4">
@@ -32,7 +49,7 @@ const Login = () => {
             type="password"
             fullWidth
             variant="outlined"
-            sx={{ mb:1 }}
+            sx={{ mb: 1 }}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -52,7 +69,12 @@ const Login = () => {
         </div>
 
         <div className="space-y-3">
-          <Button variant="outlined" sx={{ mb: 1 }} fullWidth startIcon={<GoogleIcon />}>
+          <Button
+            variant="outlined"
+            sx={{ mb: 1 }}
+            fullWidth
+            startIcon={<GoogleIcon />}
+          >
             Continue with Google
           </Button>
 
